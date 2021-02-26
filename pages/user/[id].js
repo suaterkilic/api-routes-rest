8c77a16d0d1cee5bf8f48b80dc5de1d260b0ef27
suaterkilic <76135400+suaterkilic@users.noreply.github.com>
2021-02-26 18:11:01 +0300

    Update [id].js

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -10,8 +10,7 @@ export default function User() {
     fetcher
   )
 
-  if (error) return <div>Failed to load user</div>
-  if (!data) return <div>Loading...</div>
+
 
   return <div>{data.name}</div>
 }
