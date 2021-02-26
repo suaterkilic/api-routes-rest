c345b3b25187ef34127887fb677ce54d10a881c7
suaterkilic <76135400+suaterkilic@users.noreply.github.com>
2021-02-26 18:10:35 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 export default function Index() {
   const { data, error } = useSwr('/api/users', fetcher)
 
-  if (error) return <div>Failed to load users</div>
-  if (!data) return <div>Loading...</div>
+
 
   return (
     <ul>
